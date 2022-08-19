ba1aa52d8bbc3d418c5f1368c4ff94e525c2118d
joynal67598 <joynalam67598@gmail.com>
2022-08-19 10:23:02 +0600

    js part completed

--- script.js
+++ script.js
@@ -11,11 +11,9 @@ const listPage = document.getElementById("table-div");
 const toogleBtn = document.getElementById("toogle");
 const submitBtn = document.getElementById("submit");
 
-let touristReviews = [];
-let reviewsSize = 0;
+var row = null;
 
 function showError(elem, message) {
-  elem.className = "error";
   elem.nextElementSibling.innerHTML = message;
 }
 
@@ -24,15 +22,20 @@ function clearError(elem) {
   elem.nextElementSibling.innerHTML = "";
 }
 
-function placeNameExist(placeName, id) {
-  for (i = 0; i < reviewsSize; i++) {
-    if (id != -1) {
+function verifyName(placeName) {
+  let rows = reviewTable.getElementsByTagName("tr");
+  for (i = 1; i < rows.length; i++) {
+    td = rows[i].cells[0];
+    if (td) {
+      txtValue = td.innerText;
       if (
-        touristReviews[i].placeName == placeName &&
-        id != touristReviews[i].id
+        row != null &&
+        txtValue == placeName &&
+        row.rowIndex != rows[i].rowIndex
       )
         return true;
-    } else if (touristReviews[i].placeName == placeName) return true;
+      else if (row == null && txtValue == placeName) return true;
+    }
   }
   return false;
 }
@@ -42,22 +45,26 @@ function validateForm(touristPlaceData) {
   if (touristPlaceData[0] == "") {
     valid = false;
     showError(nameField, "Name is required");
-  } else if (placeNameExist(touristPlaceData[0].trim(), touristPlaceData[5])) {
+  } else if (verifyName(touristPlaceData[0])) {
     valid = false;
-    showError(nameField, "Place name already taken");
+    showError(nameField, "Name already taken");
   } else clearError(nameField);
+
   if (touristPlaceData[1] == "") {
     valid = false;
     showError(addressField, "Address is required");
   } else clearError(addressField);
+
   if (touristPlaceData[2] < 1 || touristPlaceData[2] > 5) {
     valid = false;
     showError(ratingField, "Range 1 to 5");
   } else clearError(ratingField);
+
   if (touristPlaceData[3] == "") {
     valid = false;
     showError(typeField, "Type is required");
   } else clearError(typeField);
+
   if (touristPlaceData[4] == "") {
     valid = false;
     showError(pictureField, "Picture can't be balnk.");
@@ -74,42 +81,24 @@ function submitForm(e) {
   var touristPlaceData = getTtouristPlaceData();
   let valid = validateForm(touristPlaceData);
   if (valid) {
-    getStoredData();
-    touristReviews.push({
-      id: reviewsSize ? reviewsSize + 1 : 1,
-      placeName: touristPlaceData[0],
-      address: touristPlaceData[1],
-      rating: touristPlaceData[2],
-      type: touristPlaceData[3],
-      pictureSrc: touristPlaceData[4],
-    });
-    sessionStorage.setItem("touristReviews", JSON.stringify(touristReviews));
+    if (row == null) genarateRow(touristPlaceData);
+    else updateData();
     clearForm();
     togglePage();
   }
 }
 
 function getTtouristPlaceData() {
-  let id = reviewIdField.value;
   let placeName = nameField.value;
   let address = addressField.value;
   let rating = ratingField.value;
   let type = typeField.value;
-  let pictureSrc = pictureField.value;
+  let pictureSrc = getImageName(pictureField.value);
 
-  return [placeName, address, rating, type, pictureSrc, id];
-}
-
-function getStoredData() {
-  var previousStoredData = sessionStorage.getItem("touristReviews");
-  if (previousStoredData != null) {
-    touristReviews = JSON.parse(previousStoredData);
-    reviewsSize = touristReviews.length;
-  }
+  return [placeName, address, rating, type, pictureSrc];
 }
 
 function clearForm() {
-  reviewIdField.value = -1;
   nameField.value = "";
   addressField.value = "";
   ratingField.value = "";
@@ -117,135 +106,84 @@ function clearForm() {
   pictureField.value = "";
 }
 
-function drawTable() {
-  getStoredData();
-  for (i = 0; i < reviewsSize; i++) {
-    genarateRow(touristReviews[i]);
-  }
-}
-
-drawTable();
-
 function genarateRow(data) {
-  var newRow = reviewTable.insertRow();
-  var cell1 = newRow.insertCell();
-  var cell2 = newRow.insertCell();
-  var cell3 = newRow.insertCell();
-  var cell4 = newRow.insertCell();
-  var cell5 = newRow.insertCell();
-  cell1.innerHTML = data.placeName;
-  cell2.innerHTML = data.address;
-  cell3.innerHTML = data.rating;
-  cell4.innerHTML = `<img src="/images/${getImageName(
-    data.pictureSrc
-  )}" height="100" width="100"/>`;
-  cell5.innerHTML = `<div class="container">
-                        <a
-                          class="button"
-                          style="background-color: #0ea5c7"
-                          onclick="editData(${data.id})"
-                          >Update</a
-                        >
-                        <a
-                          class="button"
-                          style="background-color: #b52b09"
-                          onClick="deleteData(${data.id})"
-                          >Delete</a
-                        >
+  var row = reviewTable.insertRow();
+  var cell1 = row.insertCell();
+  var cell2 = row.insertCell();
+  var cell3 = row.insertCell();
+  var cell4 = row.insertCell();
+  var cell5 = row.insertCell();
+  var cell6 = row.insertCell();
+  cell1.innerHTML = data[0];
+  cell2.innerHTML = data[1];
+  cell3.innerHTML = data[2];
+  cell4.innerHTML = data[3];
+  cell4.style.display = "none";
+  cell5.innerHTML = `<img src="/images/${data[4]}" height="100" width="100"/>`;
+  cell6.innerHTML = `<div class="container">
+                        <button
+                          type = "button"
+                          class="button update-btn"
+                          onclick="editData(this)"
+                          >Update
+                        </button>
+                        <button
+                          type = "button"
+                          class="button delete-btn"
+                          onClick="deleteData(this)"
+                          >Delete
+                        </button>
                       </div>`;
 }
 
-function getReviewById(id) {
-  getStoredData();
-  let position = 0;
-  for (i = 0; i < reviewsSize; i++) {
-    if (touristReviews[i].id == id) {
-      console.log(touristReviews[i]);
-      position = i;
-      break;
-    }
-  }
-  return position;
-}
-
-function editData(id) {
-  let pos = getReviewById(id);
-  submitBtn.innerHTML = "Update";
-  submitBtn.setAttribute("onclick", "updateData(event)");
-  togglePage();
-  reviewIdField.value = touristReviews[pos].id;
-  nameField.value = touristReviews[pos].placeName;
-  addressField.value = touristReviews[pos].address;
-  ratingField.value = touristReviews[pos].rating;
-  typeField.value = touristReviews[pos].type;
+function editData(td) {
+  row = td.parentElement.parentElement.parentElement;
+  nameField.value = row.cells[0].innerHTML;
+  addressField.value = row.cells[1].innerHTML;
+  ratingField.value = row.cells[2].innerHTML;
+  typeField.value = row.cells[3].innerHTML;
   // pictureField.value = "/image/" + touristReviews[pos].pictureSrc;
   // pictureField.value = touristReviews[pos].pictureSrc;
+  togglePage();
 }
 
-function updateData(e) {
-  e.preventDefault();
-  var touristPlaceData = getTtouristPlaceData();
-  let valid = validateForm(touristPlaceData);
-  if (valid) {
-    let pos = getReviewById(touristPlaceData[5]);
-    touristReviews[pos].id = touristPlaceData[5];
-    touristReviews[pos].placeName = touristPlaceData[0];
-    touristReviews[pos].address = touristPlaceData[1];
-    touristReviews[pos].rating = touristPlaceData[2];
-    touristReviews[pos].type = touristPlaceData[3];
-    touristReviews[pos].pictureSrc = touristPlaceData[4];
-
-    sessionStorage.setItem("touristReviews", JSON.stringify(touristReviews));
-    clearForm();
-    togglePage();
-    submitBtn.innerHTML = "Submit";
-    submitBtn.setAttribute("onclick", "submitForm(event)");
-  }
+function updateData() {
+  row.cells[0].innerHTML = nameField.value;
+  row.cells[1].innerHTML = addressField.value;
+  row.cells[2].innerHTML = ratingField.value;
+  row.cells[3].innerHTML = typeField.value;
+  row.cells[4].src = pictureField.value;
+  row = null;
 }
 
-function deleteData(id) {
+function deleteData(td) {
   let ans = confirm("Are you sure delete this row?");
+  let rowIndex = td.parentElement.parentElement.parentElement.rowIndex;
   if (ans) {
-    let pos = getReviewById(id);
-    touristReviews.splice(pos, 1);
-    sessionStorage.setItem("touristReviews", JSON.stringify(touristReviews));
-    drawTable();
-  }
-}
-
-function emptyTable() {
-  while (reviewTable.rows.length > 1) {
-    reviewTable.deleteRow(1);
+    reviewTable.deleteRow(rowIndex);
   }
 }
 
 function getReviewByPlacenName() {
-  getStoredData();
-  emptyTable();
-  let inputName = serachInput.value;
-  // var found = false;
-  for (i = 0; i < reviewsSize; i++) {
-    if (touristReviews[i].placeName == inputName) {
-      genarateRow(touristReviews[i]);
-      found = true;
-      break;
+  let inputName = serachInput.value.toUpperCase();
+  let rows = reviewTable.getElementsByTagName("tr");
+  for (i = 1; i < rows.length; i++) {
+    td = rows[i].cells[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(inputName) > -1) {
+        rows[i].style.display = "";
+      } else {
+        rows[i].style.display = "none";
+      }
     }
   }
-  // if (!found) {
-  //   var newRow = reviewTable.insertRow();
-  //   var cell1 = newRow.insertCell();
-  //   cell1.colSpan = "5";
-  //   cell1.innerHTML = "No Data Found";
-  // }
 }
 
 function sortTable(n) {
   var table,
     rows,
     switching,
-    i,
-    x,
-    y,
     shouldSwitch,
     dir,
     switchcount = 0;
@@ -257,8 +195,8 @@ function sortTable(n) {
     rows = table.rows;
     for (i = 1; i < rows.length - 1; i++) {
       shouldSwitch = false;
-      x = rows[i].getElementsByTagName("TD")[n];
-      y = rows[i + 1].getElementsByTagName("TD")[n];
+      let x = rows[i].getElementsByTagName("TD")[n];
+      let y = rows[i + 1].getElementsByTagName("TD")[n];
       if (dir == "asc") {
         if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
           shouldSwitch = true;
